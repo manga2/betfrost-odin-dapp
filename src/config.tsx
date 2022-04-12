4aab705235d82505a296bff6d0e56913a7d9dae2
manga2 <codingexpert1001@gmail.com>
2022-04-11 18:31:27 -0700

    010

--- src/config.tsx
+++ src/config.tsx
@@ -4,7 +4,7 @@ export const dAppName = 'Betfrost Odin';
 export const SOCIAL_WEBSITE_URL = 'https://odindefi.app/';
 
 // Odin's Fate
-export const FLIP_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgquceuldswwlqqvtc7ndjua0smnshrdthdj9qsnemncs';
+export const FLIP_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgq9d3lnc0ft5hs6p7q5wap6cma7enlcqnwj9qs42d9yf';
 export const FLIP_CONTRACT_ABI_URL = '/abi/odin-coinflip.abi.json';
 export const FLIP_CONTRACT_NAME = 'OdinCoinflip';
 export const FLIP_GAS_LIMIT = 6000000;
