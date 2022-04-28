c17ff666ca3174e27903aabfc8087d282cca9d9f
manga2 <codingexpert1001@gmail.com>
2022-04-28 06:33:59 -0700

    fancy completed third part to show how to play

--- src/pages/GraceOfFryja/data.tsx
+++ src/pages/GraceOfFryja/data.tsx
@@ -25,3 +25,56 @@ export const rounds = [
         result: [1, 2, 5, 0]
     }
 ];
+
+import EgldLogo from 'assets/img/tokens/EGLD.svg';
+
+export const tokens = [
+    {
+        identifier: "EGLD",
+        ticker: "EGLD",
+        decimals: 18,
+        url: EgldLogo,
+    },
+    {
+        identifier: "ODIN-4d429b",
+        ticker: "ODIN",
+        decimals: 6,
+        url: "https://media.elrond.com/tokens/asset/ODIN-4d429b/logo.svg"
+    },
+    {
+        identifier: "USDC-c76f1f",
+        ticker: "USDC",
+        decimals: 6,
+        url: "https://media.elrond.com/tokens/asset/USDC-c76f1f/logo.svg"
+    },
+    {
+        identifier: "LUCKY-2cb58b",
+        ticker: "LUCKY",
+        decimals: 18,
+        url: "https://media.elrond.com/tokens/asset/LUCKY-2cb58b/logo.svg"
+    },
+    {
+        identifier: "MEX-455c57",
+        ticker: "MEX",
+        decimals: 18,
+        url: "https://media.elrond.com/tokens/asset/MEX-455c57/logo.svg"
+    },
+    {
+        identifier: "WEGLD-bd4d79",
+        ticker: "WEGLD",
+        decimals: 18,
+        url: "https://media.elrond.com/tokens/asset/WEGLD-bd4d79/logo.svg"
+    },
+    {
+        identifier: "SUPER-507aa6",
+        ticker: "SUPER",
+        decimals: 18,
+        url: "https://media.elrond.com/tokens/asset/SUPER-507aa6/logo.svg"
+    },
+    {
+        identifier: "VITAL-bc0917",
+        ticker: "VITAL",
+        decimals: 8,
+        url: "https://media.elrond.com/tokens/asset/VITAL-bc0917/logo.svg"
+    },
+];
--- src/pages/GraceOfFryja/index.tsx
+++ src/pages/GraceOfFryja/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Dropdown } from 'react-bootstrap';
 
 import './index.scss';
 import buyTicketImg from 'assets/img/Grace of Fryja/Buy Ticket.svg';
@@ -12,34 +12,68 @@ import buyTicketsButImg from 'assets/img/Grace of Fryja/Buy Tickets But.svg';
 import girl1Img from 'assets/img/Grace of Fryja/girl1.svg';
 import titleImg from 'assets/img/Grace of Fryja/title.svg';
 import whowill from 'assets/img/Grace of Fryja/whowill.svg';
+import winingCreteria from 'assets/img/Grace of Fryja/Wining Criteria.svg';
 import CountDown from './CountDown';
 
+import * as data from './data';
+
 const GraceOfFryja = () => {
-    const [tabValue, setValue] = React.useState('1');
+    const [tabValue, setTabValue] = useState('1');
+    const [CurrentRoundID, setCurrentRoundID] = useState<number>(0); // for finished rounds
 
     const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
-        setValue(newValue);
+        setTabValue(newValue);
+    };
+
+
+    /** for finished rounds */
+    const handleDecCurrentRoundID = () => {
+        if (CurrentRoundID > 0) {
+            setCurrentRoundID(CurrentRoundID - 1);
+        }
+    };
+
+    const handleIncCurrentRoundID = () => {
+        if (CurrentRoundID < data.rounds.length - 1) {
+            setCurrentRoundID(CurrentRoundID + 1);
+        }
+    };
+
+    const handleFirstCurrentRoundID = () => {
+        setCurrentRoundID(0);
+    };
+
+    const handleLastCurrentRoundID = () => {
+        setCurrentRoundID(data.rounds.length - 1);
+    };
+
+
+    const [selectedTokenId, setSelectedTokenId] = useState<number | undefined>(0);
+    const handleSelectTokenId = (token_id) => {
+        setSelectedTokenId(token_id);
     };
 
     return (
         <>
             <div style={{ background: "#121212" }}>
-                {/** first part */}
+                {/** first part : Lottery Home */}
                 <div className='fryja-first-part'>
                     <Container className='fryja-inner-container text-center' style={{ paddingTop: "100px" }}>
-                        <img className="fryja-title" src={titleImg} alt="Grace of Fryja"/>
+                        <img className="fryja-title" src={titleImg} alt="Grace of Fryja" />
                         <CountDown />
                         <div style={{ display: "flex", justifyContent: "center", textAlign: "center" }}>
-                            <div className="buy-ticket-button" >
-                                <img className="but-title" src={buyTicketImg} />
-                            </div>
+                            <a href="#buyTickets">
+                                <div className="buy-ticket-button" >
+                                    <img className="but-title" src={buyTicketImg} />
+                                </div>
+                            </a>
                         </div>
                     </Container>
                 </div>
 
-                {/** second part */}
-                <div className='fryja-second-part'>
-                    <Container style={{ paddingTop: "30px" }}>
+                {/** second part : Control Lottery */}
+                <div className='fryja-second-part' id="buyTickets">
+                    <Container style={{ paddingTop: "80px" }}>
                         <Box sx={{ width: '100%', typography: 'body1' }}>
                             <TabContext value={tabValue}>
                                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
@@ -54,8 +88,29 @@ const GraceOfFryja = () => {
                                     <Row>
                                         <Col md="5" style={{ marginTop: "20px" }}>
                                             <div className="Buy-Ticket-Box" >
-                                                <input className="custom-input" type='number' placeholder='Select Token' />
+                                                <Dropdown onSelect={handleSelectTokenId} drop='down'>
+                                                    <Dropdown.Toggle className='token-id-toggle' id="token-id">
+                                                        {
+                                                            <>
+                                                                <span>{data.tokens[selectedTokenId].ticker}</span>
+                                                                <img src={data.tokens[selectedTokenId].url} />
+                                                            </>
+                                                        }
+                                                    </Dropdown.Toggle>
+                                                    <Dropdown.Menu className='token-id-menu'>
+                                                        {
+                                                            data.tokens.map((token, index) => (
+                                                                <Dropdown.Item eventKey={index} key={`token-id-menu-item-${token.identifier}`}>
+                                                                    <span>{token.ticker}</span>
+                                                                    <img src={token.url} />
+                                                                </Dropdown.Item>
+                                                            ))
+                                                        }
+                                                    </Dropdown.Menu>
+                                                </Dropdown>
+
                                                 <input className="custom-input" type='number' placeholder='Number of Tickets' />
+
                                                 <div className="fryja-center">
                                                     <div style={{ justifyContent: "space-between", display: "flex", width: "100px" }}>
                                                         <div className="control-but">-</div>
@@ -90,59 +145,111 @@ const GraceOfFryja = () => {
 
                                     <div className="fryja-rounds fryja-center">
                                         <div style={{ color: "white" }}>
-                                            <p style={{ fontFamily: "IM FELL English SC", fontSize: "18px" }}>Draw: {"Apr 17 2022, 11:38 AM"} </p>
+                                            <p style={{ fontFamily: "IM FELL English SC", fontSize: "18px" }}>Draw: {data.rounds[CurrentRoundID].date} </p>
 
                                             <div className="fryja-center" style={{ display: "flex", gap: "20px" }}>
-                                                <div className="lottery-small-number-card">
-                                                    <span className="lottery-number">{"5"}</span>
-                                                </div>
-
-                                                <div className="lottery-small-number-card">
-                                                    <span className="lottery-number">{"2"}</span>
-                                                </div>
-
-                                                <div className="lottery-small-number-card">
-                                                    <span className="lottery-number">{"3"}</span>
-                                                </div>
-
-                                                <div className="lottery-small-number-card">
-                                                    <span className="lottery-number">{"7"}</span>
-                                                </div>
+                                                {
+                                                    data.rounds[CurrentRoundID].result.map((roundResult, index) => {
+                                                        return (
+                                                            <div className="lottery-small-number-card" key={index}>
+                                                                <span className="lottery-number">{roundResult}</span>
+                                                            </div>
+                                                        );
+                                                    })
+                                                }
                                             </div>
 
                                             <div className="fryja-center" style={{ display: "flex", gap: "20px", marginTop: "20px", marginBottom: "30px" }}>
-                                                <div className="circle-but">
+                                                <div className="circle-but" onClick={handleFirstCurrentRoundID}>
                                                     <span>{"<<"}</span>
                                                 </div>
-                                                <div className="circle-but">
+                                                <div className="circle-but" onClick={handleDecCurrentRoundID}>
                                                     <span>{"<"}</span>
                                                 </div>
-                                                <div className="circle-but">
+                                                <div className="circle-but" onClick={handleIncCurrentRoundID}>
                                                     <span>{">"}</span>
                                                 </div>
-                                                <div className="circle-but">
+                                                <div className="circle-but" onClick={handleLastCurrentRoundID}>
                                                     <span>{">>"}</span>
                                                 </div>
                                             </div>
 
-                                            <p style={{ fontFamily: "IM FELL English SC", fontSize: "18px", color: "#BDBDBD", marginBottom: "30px" }}>Finished Round: {"#2"} </p>
+                                            <p style={{ fontFamily: "IM FELL English SC", fontSize: "18px", color: "#BDBDBD", marginBottom: "30px" }}>Finished Round: #{CurrentRoundID + 1} </p>
 
-                                            <img src={whowill} style={{ width: "100%" }} alt="who will recieve the grace of fryja"/>
+                                            <img src={whowill} style={{ width: "100%" }} alt="who will recieve the grace of fryja" />
                                         </div>
                                     </div>
 
                                 </TabPanel>
 
                                 {/** tab for lottery histories */}
-                                <TabPanel value="2">Item Two</TabPanel>
+                                <TabPanel value="2">
+                                    <p>{"If the digits on your tickets match the winning numbers in the correct order, you win a portion of the prize pool."}</p>
+                                </TabPanel>
                             </TabContext>
                         </Box>
                     </Container>
                 </div>
 
-                <div className='fryja-third-part'>
-                    <Container style={{ paddingTop: "30px" }}>
+                {/** Third Part : How to play */}
+                <div className='fryja-third-part pb-5'>
+                    <Container style={{ paddingTop: "60px" }}>
+                        <div className="text-center">
+                            <p style={{ color: "#616161" }}>{"If the digits on your tickets match the winning numbers in the correct order, you win a portion of the prize pool."}</p>
+                        </div>
 
+                        <div className="info-box">
+                            <Row>
+                                <Col sm="4">
+                                    <p className="step-info-title">Buy Tickets</p>
+                                    <p className="step-info-description">{"Prices are set when the round starts, equal to 50 USD in Odin per ticket."}</p>
+                                </Col>
+
+                                <Col sm="4">
+                                    <p className="step-info-title">Wait for the Draw</p>
+                                    <p className="step-info-description">{"There is one draw every day alternating between 0 AM UTC and 12 PM UTC."}</p>
+                                </Col>
+
+                                <Col sm="4">
+                                    <p className="step-info-title">Check for Prizes</p>
+                                    <p className="step-info-description">{"Once the round’s over, come back to the page and check to see if you’ve won!"}</p>
+                                </Col>
+                            </Row>
+                        </div>
+
+                        <div className="info-box mt-5">
+                            <Row style={{ alignItems: "center" }}>
+                                <Col sm="8">
+                                    <p className="step-info-title" style={{ fontWeight: "700" }}>Winning Criteria</p>
+                                    <p className="step-info-title" style={{ fontWeight: "500", fontSize: "17px" }}>{"The digits on your ticket must match in the correct order to win."}</p>
+                                    <p className="step-info-description">
+                                        {"Here’s an example lottery draw, with two tickets, A and B."} <br />
+                                        {"- Ticket A: The first 2 digits and the last 1 digit match, but the 3th digit is wrong, so this ticket only wins a “Match first 2” prize"} <br />
+                                        {"- Ticket B: Even though the last 3 digits match, the first digit is wrong, so this ticket doesn’t win a prize."} <br />
+                                        {"Prize brackets don’t ‘stack’: if you match the first 3 digits in order, you’ll only win prizes from the ‘Match 3’ bracket, and not from ‘Match 1’ and ‘Match 2’."}</p>
+                                </Col>
+
+                                <Col sm="4">
+                                    <div className='fryja-center'>
+                                        <img className="w-100" src={winingCreteria} alt="wining creteria" />
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+
+                        <div className="info-box mt-5">
+                            <p className="step-info-title" style={{ fontWeight: "700" }}>Prize Funds</p>
+                            <p className="step-info-description">{"The prizes for each lottery round come from three sources:"}</p>
+
+                            <span className="step-info-title" style={{ fontWeight: "500", fontSize: "17px" }}>{"Ticket Purchases"}</span>
+                            <p className="step-info-description">{"- 100% of the CAKE paid by people buying tickets that round goes back into the prize pools."}</p>
+
+                            <span className="step-info-title" style={{ fontWeight: "500", fontSize: "17px" }}>{"Rollover Prizes"}</span>
+                            <p className="step-info-description">{"- After every round, if nobody wins in one of the prize brackets, the unclaimed CAKE for that bracket rolls over into the next round and are redistributed among the prize pools."}</p>
+
+                            <span className="step-info-title" style={{ fontWeight: "500", fontSize: "17px" }}>{"CAKE Injections"}</span>
+                            <p className="step-info-description">{"- An average total of 35,000 CAKE from the treasury is added to lottery rounds over the course of a week. This CAKE is of course also included in rollovers! Read more in our guide to CAKE Tokenomics"}</p>
+                        </div>
                     </Container>
                 </div>
             </div>
